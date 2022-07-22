4057aec38526667dc024a61b48182aeb227064e3
Myeong <sowkosowko@naver.com>
2022-07-23 04:35:08 +0900

    start B & W

---
+++ B-and-W/src/main/java/start/main/BAndWApplication.java
@@ -0,0 +1,13 @@
+package start.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BAndWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BAndWApplication.class, args);
+	}
+
+}
