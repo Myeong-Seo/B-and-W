02a2440c4fc53d53dafc692a98807bf7af0bdd8b
Myeong <sowkosowko@naver.com>
2022-07-25 15:09:11 +0900

    2022-07-25 finish

--- B-and-W/src/main/java/start/main/controller/MainController.java
+++ B-and-W/src/main/java/start/main/controller/MainController.java
@@ -1,13 +1,19 @@
 package start.main.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 	
 	@GetMapping("/main")
 	public String main() {
 		return "main";
+	}
+	@GetMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("loginpage","B & W 로그인");
+		return "login";
 	}
 }
