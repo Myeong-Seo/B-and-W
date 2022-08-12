8e580e59480290cf5770047848e4214e7da8e832
Myeong <sowkosowko@naver.com>
2022-08-12 17:29:46 +0900

    2022-08-12 finish

--- B-and-W/src/main/java/start/main/controller/LoginRestController.java
+++ B-and-W/src/main/java/start/main/controller/LoginRestController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,10 +48,10 @@ public class LoginRestController {
 	//회원 세션
 	@PostMapping("/login_user")
 	public void login(@RequestParam("id") String id, @RequestParam("password") String password, HttpSession session, HttpServletResponse response) {
-		System.out.println(1);
 		UserVO user = us.userinfo(id);
 		
 		session.setAttribute("id", user.getUid());
+		session.setAttribute("password", user.getUpwd());
 		session.setAttribute("email", user.getUmail());
 		session.setAttribute("number", user.getUnumber());
 		
@@ -59,7 +60,30 @@ public class LoginRestController {
 			response.sendRedirect(redirectUrl);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}	
+		}
+	}
+	//회원정보 세션
+	@PostMapping("/modify_user")
+	public void modify(@RequestParam("id") String id, @RequestParam("password1") String password1, @RequestParam("password2") String password2, @RequestParam("email") String email, @RequestParam("number") String number, HttpServletResponse response, HttpSession session) {
+		UserVO user = us.userinfo(id);
+		
+		if(user.getUpwd().equals(password1)) {
+			user.setUmail(email);
+			user.setUnumber(number);
+			session.setAttribute("id", user.getUid());
+			session.setAttribute("password", user.getUpwd());
+			session.setAttribute("email", user.getUmail());
+			session.setAttribute("number", user.getUnumber());
+			us.join_Member(user);
+			session.invalidate();
+		}
+		String redirectUrl = "/main";
+		try {
+			response.sendRedirect(redirectUrl);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 
---
+++ B-and-W/src/main/resources/static/js/user_info.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+	document.getElementById("modify").addEventListener("click", function() {
+		return alert("회원정보 변경이 완료되었습니다. 다시 로그인 해주세요.");
+	})
+}
